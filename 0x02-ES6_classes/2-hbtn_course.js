2f3310f029eadc73058ce3be38018c4db14ef3f7
abdisag1 <myabdisa@gmail.com>
2022-06-16 23:45:01 +0300

    fixed task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,50 +1,65 @@
+// Implement a class named HolbertonCourse:
+// Constructor attributes:
+// name (String)
+// length (Number)
+// students (array of Strings)
+// Make sure to verify the type of attributes during object creation
+// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+// Implement a getter and setter for each attribute.
+
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    // name (String)
     if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new Error('name must be a string');
     }
     this._name = name;
+
+    // length (Number)
     if (typeof length !== 'number') {
-      throw new TypeError('length must be a number');
+      throw new Error('length must be a number');
     }
     this._length = length;
-    if (!Array.isArray(students)) {
-      throw new TypeError('students must be an array');
+
+    // students (array of Strings)
+    if (typeof students !== 'object') {
+      throw new Error('students must be an array');
     }
 
     this._students = students;
   }
 
+  // Implement a getter and setter for each attribute.
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('name must be a string');
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new Error('name must be a string');
     }
-    this._name = value;
+    this._name = name;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('length must be a number');
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new Error('length must be a number');
     }
-    this._length = value;
+    this._length = length;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    if (!Array.isArray(value)) {
-      throw new TypeError('students must be an array');
+  set students(students) {
+    if (typeof students !== 'object') {
+      throw new Error('students must be an array');
     }
-    this._students = value;
+    this._students = students;
   }
 }
