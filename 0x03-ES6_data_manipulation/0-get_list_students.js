64062403e5822b17d135f89e1d782dee6ab2d602
abdisag1 <myabdisa@gmail.com>
2022-06-20 20:40:21 +0300

    done task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const array = [
+    {
+      id: 1,
+      firstNmae: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstNmae: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 3,
+      firstNmae: 'Serena',
+      location: 'San Francisco',
+    },
+
+  ];
+  return array;
+}
