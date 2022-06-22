4a09298a3f0c6fca6aa1f886a0b05c45fa617c8e
abdisag1 <myabdisa@gmail.com>
2022-06-22 05:59:37 +0300

    fixed task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,14 @@
+/* Create a function cleanSet
+   REturns startString and accepts two args */
 export default function cleanSet(set, startString) {
-  const list = [];
-  for (const element of set) {
-    list.push(element.slice(startString.length));
+  if (!startString || typeof startString !== 'string') return '';
+
+  const strList = [];
+  for (const str of set) {
+    if (typeof str === 'string' && str.startsWith(startString)) {
+      strList.push(str.slice(startString.length));
+    }
   }
-  return list.join('-');
+
+  return strList.join('-');
 }
