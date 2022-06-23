9e27158e80a283c276018ec06ab164a68ff15a47
abdisag1 <myabdisa@gmail.com>
2022-06-23 04:55:44 +0300

    ambiant namespace

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,9 @@
+import { RowID, RowElement } from "./interface";
+import * as CRUD from './crud';
+
+const row: RowElement = { firstName: 'Miguel', lastName: 'Pacheco', };
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
